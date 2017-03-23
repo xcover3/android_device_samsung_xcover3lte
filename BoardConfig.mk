493e64abe6b2094d465c90cdc2be3495dcca0cd4
Matt07211 <matthew.hanford2@gmail.com>
2017-03-23 22:59:13 +1100

    Revert "mkvendor.sh oversight"
    This reverts commit fe1e88dde3f9d284311ea350fa603cc4ca09b9f2.
    
    I screwed up the previous commit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,7 +13,7 @@ TARGET_SOC := pxa1908
 -include vendor/samsung/xcover3lte/BoardConfigVendor.mk
 
 # Architecture/Bootloader
-TARGET_ARCH := arm64
+TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
